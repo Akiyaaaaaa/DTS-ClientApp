68077750b1da5b47e4cefe155b53d3934aee699c
johan <johanwirasugianto@gmail.com>
2023-05-03 10:37:11 +0700

    Inizial Client App

---
+++ src/main/java/com/mii/clientapp/controller/HomeController.java
@@ -0,0 +1,21 @@
+package com.mii.clientapp.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+    @GetMapping
+    public String home(Model model){
+        model.addAttribute("name","Johan");
+        return "index";
+    }
+
+}
