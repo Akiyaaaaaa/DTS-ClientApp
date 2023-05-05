7e720ae4f0293267c78bfbd0ce0624cb8c26b5bd
johan <johanwirasugianto@gmail.com>
2023-05-05 16:29:43 +0700

    Set Logic login

--- src/main/java/com/mii/clientapp/controller/LoginController.java
+++ src/main/java/com/mii/clientapp/controller/LoginController.java
@@ -1,14 +1,30 @@
 package com.mii.clientapp.controller;
 
+import com.mii.clientapp.model.dto.request.LoginRequest;
+import com.mii.clientapp.service.LoginService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/login")
+@AllArgsConstructor
 public class LoginController {
 
-    @GetMapping("/login")
-    public String login(){
+    private LoginService loginService;
+
+    @GetMapping
+    public String loginView(LoginRequest loginRequest){
         return "auth/login";
     }
 
+    @PostMapping
+    public String login(LoginRequest loginRequest){
+        if (!loginService.login(loginRequest)){
+            return "redirect:/login?error=true";
+        }
+        return "redirect:/region";
+    }
 }
---
+++ src/main/java/com/mii/clientapp/service/LoginService.java
@@ -0,0 +1,59 @@
+package com.mii.clientapp.service;
+
+import com.mii.clientapp.model.dto.request.LoginRequest;
+import com.mii.clientapp.model.dto.response.LoginResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class LoginService {
+
+    private RestTemplate restTemplate;
+
+    @Autowired
+    public LoginService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @Value("${server.baseUrl}/login")
+    private String url;
+
+    public Boolean login(LoginRequest loginRequest){
+        ResponseEntity<LoginResponse> res = restTemplate.exchange(
+                url,
+                HttpMethod.POST,
+                new HttpEntity(loginRequest),
+                new ParameterizedTypeReference<LoginResponse>() {
+                }
+        );
+        if (res.getStatusCode() == HttpStatus.OK){
+            setAuthentication(res.getBody());
+            return true;
+        }
+        return false;
+    }
+
+    public void setAuthentication(LoginResponse res){
+        List<SimpleGrantedAuthority> authorities = res.getAuthorities().stream()
+                .map(authority -> new SimpleGrantedAuthority(authority))
+                .collect(Collectors.toList());
+
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(res.getUsername(), null, authorities);
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+    }
+
+}
