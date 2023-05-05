6fe08623268658fd1138c8b396cc355e65ff017f
johan <johanwirasugianto@gmail.com>
2023-05-05 14:56:12 +0700

    Initialization Spring Security and Custome Login

--- src/main/java/com/mii/clientapp/service/RegionService.java
+++ src/main/java/com/mii/clientapp/service/RegionService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -20,11 +21,13 @@ public class RegionService {
   private String url;
 
   public List<Region> getAll() {
+    HttpHeaders headers = new HttpHeaders();
+    headers.add("Authorization","basic am9oYW46am9oYW5wYXNz"); // johan:johanpass
     return restTemplate
       .exchange(
         url,
         HttpMethod.GET,
-        null,
+        new HttpEntity(headers),
         new ParameterizedTypeReference<List<Region>>() {}
       )
       .getBody();
